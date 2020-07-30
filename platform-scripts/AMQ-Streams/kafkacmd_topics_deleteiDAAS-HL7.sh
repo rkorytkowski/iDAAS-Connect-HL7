918186191201b76e6f218b5e167c4c5a9412849c
Alan Scott <alscott@redhat.com>
2020-07-30 15:36:24 -0500

    Code consolidation from iDAAS Connect Clinical Industry Stds and reducing code to remove FHIR transactions to implement in seperate repository.

---
+++ platform-scripts/AMQ-Streams/kafkacmd_topics_deleteiDAAS-HL7.sh
@@ -0,0 +1,50 @@
+cd /Users/developer/RedHatTech/kafka_2.12-2.4.0.redhat-00005
+## Operational Topics for Platform
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic opsMgmt_PlatformTransactions &
+## HL7
+## Inbound to iDAAS Platform by Message Trigger
+## Facility: MCTN
+## Application: MMS
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MCTN_MMS_ADT &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MCTN_MMS_ORM &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MCTN_MMS_ORU &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MCTN_MMS_RDE &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MCTN_MMS_SCH &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MCTN_MMS_VXU &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MCTN_MMS_MFN &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MCTN_MMS_MDM &
+## HL7
+## Facility By Application by Message Trigger
+## Facility: MCTN
+## Application: MMS
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MCTN_ADT &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MCTN_ORM &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MCTN_ORU &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MCTN_RDE &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MCTN_SCH &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MCTN_VXU &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MCTN_MFN &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MCTN_MDM &
+## HL7
+## Enterprise By Application by Message Trigger
+## Facility: MCTN
+## Application: MMS
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MMS_ADT &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MMS_ORM &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MMS_ORU &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MMS_RDE &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MMS_SCH &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MMS_VXU &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MMS_MFN &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic MMS_MDM &
+## HL7
+## Enterprise by Message Trigger
+## Application: MMS
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ENT_ADT &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ENT_ORM &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ENT_ORU &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ENT_RDE &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ENT_SCH &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ENT_VXU &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ENT_MFN &
+bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ENT_MDM &
---
+++ platform-scripts/AMQ-Streams/start_Kafka.sh
@@ -0,0 +1,6 @@
+#_mydir='/Users/alscott/RedHatTech/kafka_2.12-2.4.0.redhat-00005'
+_mydir='/Users/developer/RedHatTech/kafka_2.12-2.4.0.redhat-00005'
+
+cd $_mydir
+bin/zookeeper-server-start.sh config/zookeeper.properties &
+bin/kafka-server-start.sh config/server.properties &
